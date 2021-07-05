fd8f87788055fe63f2160ce2e4f350b49f844e83
Sarikadki <ssevillanogarcia@gmail.com>
2021-07-05 10:19:22 +0200

    Segundo commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-package control;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hola Jenkins");
+System.out.println("Holita");
 	}
 
 }
